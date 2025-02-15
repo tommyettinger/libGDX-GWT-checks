28743c75e780dfc2804f4a1a818949ca9477adfc
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-02-14 20:31:20 -0800

    Just getting this ready...

---
+++ UBJsonCheck/core/src/main/java/com/github/tommyettinger/Main.java
@@ -0,0 +1,39 @@
+package com.github.tommyettinger;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture image;
+    private BitmapFont font;
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+        font = new BitmapFont();
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        font.draw(batch, "OK, let's see if this works...", 140, 180, 500, Align.topLeft, true);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+        font.dispose();
+    }
+}
